f225873414a1920cab66915189b3c683497fd738
yang2507366 <yang3800650071@163.com>
2012-06-28 10:46:05 +0800

    c

---
+++ Common/Utils/KeyValueManager/impl/DBKeyValueManager.h
@@ -0,0 +1,23 @@
+//
+//  DBKeyValueCache.h
+//  imyvoa
+//
+//  Created by yzx on 12-5-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KeyValueCache.h"
+#import "sqlite3.h"
+
+@interface DBKeyValueManager : NSObject <KeyValueManager> {
+    NSString *_cacheName;
+    NSString *_dbFilePath;
+    
+    sqlite3 *_db;
+}
+
+- (id)initWithDBName:(NSString *)cacheName;
+- (id)initWithDBName:(NSString *)cacheName atFilePath:(NSString *)filePath;
+
+@end
